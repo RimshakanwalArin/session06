1662c6919ad5f1d153d50c19c80ae38e1a4a7b91
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-25 14:11:08 +0500

    Add files via upload

---
+++ arithmetic_oparater.js
@@ -0,0 +1,7 @@
+// arithmetic  operater
+var num1 = 10;
+var num2 = 2;
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+console.log("this is a 10 quotiant 2" + $, { num1: num1 }, $, { num2: num2 });
---
+++ arithmetic_oparater.ts
@@ -0,0 +1,9 @@
+// arithmetic  operater
+
+let num$ = 10;
+let number = 2;
+
+console.log(num$+number);
+console.log(num$-number);
+console.log(num$*number);
+
